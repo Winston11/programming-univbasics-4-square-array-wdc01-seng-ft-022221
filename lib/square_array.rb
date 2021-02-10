2829a6aadae833c7aaf9ce81edf75545c8329ec3
Winston McGriff <winstonmcgriff27@gmail.com>
2021-02-10 15:25:28 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,9 @@
 def square_array(array)
   array = [1, 2, 3]
-  new_array = [9, 10, 16, 25]
   array.map do |element|
+    element*element
+  new_array = [9, 10, 16, 25]
+  new_array.map do |element|
     element*element
 end
 end
